6c33e6ad673bc576e4d63267d2f93faccadea004
AlaminDolar <amin15-9898@diu.edu.bd>
2022-11-11 15:30:33 +0600

    ok

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId,
+  ObjectID,
+} = require("mongodb");
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -26,6 +31,30 @@ async function run() {
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    console.log("connected");
+    // single data find
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectID(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // POST
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    // Delete
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
     });
   } finally {
     // await client.close()
